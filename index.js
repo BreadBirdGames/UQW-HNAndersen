8fe4a7951b4087190c7bded2c1c38b4c90b3cc7a
cornelius <macrosedk@gmail.com>
2025-04-09 22:33:28 +0200

    Make tabs work

--- index.js
+++ index.js
@@ -1,3 +1,60 @@
+// ####### Rest of scene #######
+
+// create QRVideo
+const qrVideo = document.createElement("video");
+qrVideo.id = "qr-video"
+
+// Import font awesome
+const fontAwesomeLink = document.createElement("link");
+fontAwesomeLink.rel = "stylesheet";
+fontAwesomeLink.href = "font-awesome/css/all.min.css"
+document.head.appendChild(fontAwesomeLink);
+
+const tabL = document.createElement("div");
+tabL.id = "left-tab";
+tabL.classList.add("tabs");
+
+const tabR = document.createElement("div");
+tabR.id = "right-tab";
+tabR.classList.add("tabs");
+tabR.appendChild(qrVideo);
+
+// Create bottom nav bar
+const buttonContainer = document.createElement("div");
+buttonContainer.id = "button-container";
+
+const logButton = document.createElement("button");
+logButton.id = "log-button";
+logButton.innerHTML = '<i class="fa-solid fa-scroll"></i>';
+// Log button click
+function logClick() {
+    if (document.body.classList.contains("current-is-right")) {
+        document.body.classList.remove("current-is-right");
+        document.body.classList.add("current-is-left");
+    }
+}
+logButton.addEventListener("click", logClick);
+buttonContainer.appendChild(logButton);
+
+const cameraButton = document.createElement("button");
+cameraButton.id = "camera-button";
+cameraButton.innerHTML = '<i class="fa-solid fa-camera-retro"></i>';
+// Camera button click
+function cameraClick() {
+    if (document.body.classList.contains("current-is-left")) {
+        document.body.classList.remove("current-is-left");
+        document.body.classList.add("current-is-right");
+    }
+}
+cameraButton.addEventListener("click", cameraClick);
+buttonContainer.appendChild(cameraButton);
+
+document.body.appendChild(buttonContainer);
+document.body.appendChild(tabL);
+document.body.appendChild(tabR);
+
+document.body.classList.add("current-is-right");
+
 // ####### Web Cam Scanning #######
 
 import QrScanner from "/qr-scanner.min.js";
@@ -10,11 +67,7 @@ function setResult(result) {
     // }
 }
 
-// Create and initialize QR video
-const qrVideo = document.createElement("video");
-qrVideo.id = "qr-video";
-document.body.appendChild(qrVideo);
-
+// initialize QR video;
 const scanner = new QrScanner(qrVideo, result => setResult(result), {
     onDecodeError: error => {},
     highlightScanRegion: true,
@@ -22,43 +75,3 @@ const scanner = new QrScanner(qrVideo, result => setResult(result), {
 });
 
 scanner.start();
-
-// Import font awesome
-const fontAwesomeLink = document.createElement("link");
-fontAwesomeLink.rel = "stylesheet";
-fontAwesomeLink.href = "font-awesome/css/all.min.css"
-document.head.appendChild(fontAwesomeLink);
-
-// Create bottom nav bar
-const buttonContainer = document.createElement("div");
-buttonContainer.id = "button-container";
-
-const logButton = document.createElement("button");
-logButton.id = "log-button";
-logButton.innerHTML = '<i class="fa-solid fa-scroll"></i>';
-// Log button click
-function logClick() {
-    if (cameraButton.classList.contains("current")) {
-        logButton.classList.add("current");
-        cameraButton.classList.remove("current");
-    }
-}
-logButton.addEventListener("click", logClick);
-buttonContainer.appendChild(logButton);
-
-const cameraButton = document.createElement("button");
-cameraButton.id = "camera-button";
-cameraButton.innerHTML = '<i class="fa-solid fa-camera-retro"></i>';
-// Camera button click
-function cameraClick() {
-    if (logButton.classList.contains("current")) {
-        logButton.classList.remove("current");
-        cameraButton.classList.add("current");
-    }
-}
-cameraButton.addEventListener("click", cameraClick);
-buttonContainer.appendChild(cameraButton);
-
-document.body.appendChild(buttonContainer);
-
-cameraButton.classList.add("current");
