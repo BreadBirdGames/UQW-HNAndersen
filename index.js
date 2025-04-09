09ae0657ee4588a699e55d814530a7d47b15cd15
cornelius <macrosedk@gmail.com>
2025-04-09 22:21:38 +0200

    Add colors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 import QrScanner from "/qr-scanner.min.js";
 
+// Function for setting result 
 function setResult(result) {
     // if (result.data in characters.keys()) {
     window.Engine.play("Admiral");
@@ -9,29 +10,10 @@ function setResult(result) {
     // }
 }
 
-const fontAwesomeLink = document.createElement("link");
-fontAwesomeLink.rel = "stylesheet";
-fontAwesomeLink.href = "font-awesome/css/all.min.css"
-document.head.appendChild(fontAwesomeLink);
-
+// Create and initialize QR video
 const qrVideo = document.createElement("video");
 qrVideo.id = "qr-video";
 document.body.appendChild(qrVideo);
-
-const buttonContainer = document.createElement("div");
-buttonContainer.id = "button-container";
-
-const logButton = document.createElement("button");
-logButton.id = "log-button";
-logButton.innerHTML = '<i class="fa-solid fa-scroll"></i>';
-buttonContainer.appendChild(logButton);
-
-const cameraButton = document.createElement("button");
-cameraButton.id = "camera-button";
-cameraButton.innerHTML = '<i class="fa-solid fa-camera-retro"></i>';
-buttonContainer.appendChild(cameraButton);
-
-document.body.appendChild(buttonContainer);
 
 const scanner = new QrScanner(qrVideo, result => setResult(result), {
     onDecodeError: error => {},
@@ -40,3 +22,43 @@ const scanner = new QrScanner(qrVideo, result => setResult(result), {
 });
 
 scanner.start();
+
+// Import font awesome
+const fontAwesomeLink = document.createElement("link");
+fontAwesomeLink.rel = "stylesheet";
+fontAwesomeLink.href = "font-awesome/css/all.min.css"
+document.head.appendChild(fontAwesomeLink);
+
+// Create bottom nav bar
+const buttonContainer = document.createElement("div");
+buttonContainer.id = "button-container";
+
+const logButton = document.createElement("button");
+logButton.id = "log-button";
+logButton.innerHTML = '<i class="fa-solid fa-scroll"></i>';
+// Log button click
+function logClick() {
+    if (cameraButton.classList.contains("current")) {
+        logButton.classList.add("current");
+        cameraButton.classList.remove("current");
+    }
+}
+logButton.addEventListener("click", logClick);
+buttonContainer.appendChild(logButton);
+
+const cameraButton = document.createElement("button");
+cameraButton.id = "camera-button";
+cameraButton.innerHTML = '<i class="fa-solid fa-camera-retro"></i>';
+// Camera button click
+function cameraClick() {
+    if (logButton.classList.contains("current")) {
+        logButton.classList.remove("current");
+        cameraButton.classList.add("current");
+    }
+}
+cameraButton.addEventListener("click", cameraClick);
+buttonContainer.appendChild(cameraButton);
+
+document.body.appendChild(buttonContainer);
+
+cameraButton.classList.add("current");
