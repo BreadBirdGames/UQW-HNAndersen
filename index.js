038b7ee5973cec5f811c9969f0b9e4bf915f1127
rosenaa <macrosedk@gmail.com>
2025-06-10 20:14:55 +0200

    Rectify a few issues

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 // This should only be used for characters, keep scene sprites in another system
 const thumbnails = {
-    "HNAndersen": "img/Kaptajn.png",
-    "Sailor" : "img/Matros.jpg",
-    "End Game" : "img/BreadBirdGames.png"
-}
+  HNAndersen: "img/Kaptajn.png",
+  Sailor: "img/Matros.jpg",
+};
 
 const characters = {
-    "Person0": "HNAndersen",
-    "Person1": "Sailor",
-    "Person2": undefined,
-    "Person3": undefined,
-    "Person4": undefined,
-    "Person5": undefined,
-    "Person6": undefined,
-    "Person7": undefined,
-    "Person8": undefined,
-    "Person9": undefined
-}
+  Person0: "HNAndersen",
+  Person1: "Sailor",
+  Person2: undefined,
+  Person3: undefined,
+  Person4: undefined,
+  Person5: undefined,
+  Person6: undefined,
+  Person7: undefined,
+  Person8: undefined,
+  Person9: undefined,
+};
 
 window.conversing = false;
 
@@ -25,14 +24,14 @@ thumbnailImage.id = "thumbnail";
 document.body.appendChild(thumbnailImage);
 
 function display_thumbnail(str) {
-    if (!window.ready) {
-        return;
-    }
-    
-    if (str in thumbnails) {
-        thumbnailImage.classList.remove("hidden");
-        thumbnailImage.src = thumbnails[str];
-    }
+  if (!window.ready) {
+    return;
+  }
+
+  if (str in thumbnails) {
+    thumbnailImage.classList.remove("hidden");
+    thumbnailImage.src = thumbnails[str];
+  }
 }
 
 window.display_thumbnail = display_thumbnail;
@@ -41,26 +40,26 @@ const virtRoot = document.createElement("div");
 
 // create QRVideo
 const qrVideo = document.createElement("video");
-qrVideo.id = "qr-video"
+qrVideo.id = "qr-video";
 
 // Import font awesome
 const fontAwesomeLink = document.createElement("link");
 fontAwesomeLink.rel = "stylesheet";
-fontAwesomeLink.href = "font-awesome/css/all.min.css"
+fontAwesomeLink.href = "font-awesome/css/all.min.css";
 document.head.appendChild(fontAwesomeLink);
 
 // Reset button!
 const resetButton = document.createElement("button");
 resetButton.id = "reset-button";
-resetButton.addEventListener("click", function() {
-    window.Engine.restart();
+resetButton.addEventListener("click", function () {
+  window.Engine.restart();
 });
 resetButton.innerHTML = "RESET GAME PROGRESS";
 
 const errorResetButton = document.createElement("button");
 errorResetButton.id = "error-reset-button";
-errorResetButton.addEventListener("click", function() {
-    window.Engine.restart();
+errorResetButton.addEventListener("click", function () {
+  window.Engine.restart();
 });
 errorResetButton.innerHTML = "RESET GAME PROGRESS";
 
@@ -75,12 +74,29 @@ window.currentObjective.innerText = "objective.";
 
 const notifcationSound = new Audio("./NotificationSound.wav");
 
-window.updateObjective = function(newObjective) {
-    window.currentObjective.innerText = newObjective;
+window.updateObjective = function (newObjective) {
+  window.currentObjective.innerText = newObjective;
 
-    notifcationSound.play();
-    setTimeout(function(){alert(newObjective);},1200);
-}
+  notifcationSound.play();
+  new Notify({
+    status: "info",
+    title: "Quest update",
+    text: newObjective,
+    effect: "fade",
+    speed: 300,
+    customClass: "",
+    customIcon: "",
+    showIcon: true,
+    showCloseButton: true,
+    autoclose: true,
+    autotimeout: 3000,
+    notificationsGap: null,
+    notificationsPadding: null,
+    type: "outline",
+    position: "center",
+    customWrapper: "",
+  });
+};
 
 const tabL = document.createElement("div");
 tabL.id = "left-tab";
@@ -103,14 +119,14 @@ logButton.id = "log-button";
 logButton.innerHTML = '<i class="fa-solid fa-scroll"></i>';
 // Log button click
 function logClick() {
-    if (!window.ready) {
-        return;
-    }
+  if (!window.ready) {
+    return;
+  }
 
-    if (virtRoot.classList.contains("current-is-right")) {
-        virtRoot.classList.remove("current-is-right");
-        virtRoot.classList.add("current-is-left");
-    }
+  if (virtRoot.classList.contains("current-is-right")) {
+    virtRoot.classList.remove("current-is-right");
+    virtRoot.classList.add("current-is-left");
+  }
 }
 logButton.addEventListener("click", logClick);
 buttonContainer.appendChild(logButton);
@@ -120,14 +136,14 @@ cameraButton.id = "camera-button";
 cameraButton.innerHTML = '<i class="fa-solid fa-camera-retro"></i>';
 // Camera button click
 function cameraClick() {
-    if (!window.ready) {
-        return;
-    }
+  if (!window.ready) {
+    return;
+  }
 
-    if (virtRoot.classList.contains("current-is-left")) {
-        virtRoot.classList.remove("current-is-left");
-        virtRoot.classList.add("current-is-right");
-    }
+  if (virtRoot.classList.contains("current-is-left")) {
+    virtRoot.classList.remove("current-is-left");
+    virtRoot.classList.add("current-is-right");
+  }
 }
 cameraButton.addEventListener("click", cameraClick);
 buttonContainer.appendChild(cameraButton);
@@ -144,22 +160,22 @@ document.body.appendChild(virtRoot);
 
 import QrScanner from "./qr-scanner.min.js";
 
-// Function for setting result 
+// Function for setting result
 function setResult(result) {
-    if (!window.ready) {
-        return;
-    }
+  if (!window.ready) {
+    return;
+  }
 
-    if (characters[result.data] != undefined) {
-        window.conversationPlay(characters[result.data]);
-    }
+  if (characters[result.data] != undefined) {
+    window.conversationPlay(characters[result.data]);
+  }
 }
 
 // initialize QR video;
-const scanner = new QrScanner(qrVideo, result => setResult(result), {
-    onDecodeError: error => {},
-    highlightScanRegion: true,
-    highlightCodeOutline: true
+const scanner = new QrScanner(qrVideo, (result) => setResult(result), {
+  onDecodeError: (error) => {},
+  highlightScanRegion: true,
+  highlightCodeOutline: true,
 });
 
 scanner.start();
@@ -168,33 +184,33 @@ scanner.start();
 
 const dialogueBox = document.getElementById("story");
 
-window.conversationPlay = function(passageName) {
-    if (window.conversing) {
-        return
-    }
+window.conversationPlay = function (passageName) {
+  if (window.conversing) {
+    return;
+  }
 
-    window.Engine.play(passageName)
+  window.Engine.play(passageName);
 };
 
 // hide dialogue box if empty
 window.conversationStart = function (ev) {
-    if (ev == undefined || ev["passage"]["name"] == "End conversation") {
-        dialogueBox.classList.add("hidden");
-        thumbnailImage.src = "";
-        thumbnailImage.classList.add("hidden");
-        window.conversing = false;
-    } else {
-        dialogueBox.classList.remove("hidden");
-        thumbnailImage.classList.remove("hidden");
-        window.conversing = true;
+  if (ev == undefined || ev["passage"]["name"] == "End conversation") {
+    dialogueBox.classList.add("hidden");
+    thumbnailImage.src = "";
+    thumbnailImage.classList.add("hidden");
+    window.conversing = false;
+  } else {
+    dialogueBox.classList.remove("hidden");
+    thumbnailImage.classList.remove("hidden");
+    window.conversing = true;
 
-        if (thumbnails[ev["passage"]["name"]] != undefined) {
-            window.display_thumbnail(ev["passage"]["name"]);
-        }
+    if (thumbnails[ev["passage"]["name"]] != undefined) {
+      window.display_thumbnail(ev["passage"]["name"]);
     }
+  }
 
-    var errorViews = document.getElementsByClassName("error-view");
-    if (errorViews.length > 0) {
-        errorViews[0].appendChild(errorResetButton);
-    }
+  var errorViews = document.getElementsByClassName("error-view");
+  if (errorViews.length > 0) {
+    errorViews[0].appendChild(errorResetButton);
+  }
 };
